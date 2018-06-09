85344d25283846f1f06386a88e8170488ee804c3
Gabriela Alves da Gama <gabrielaalvesdagama@gmail.com>
2018-06-09 20:14:16 -0300

    #38 #39 Arrumando detalhes no script de deploy

--- utils/deploy.sh
+++ utils/deploy.sh
@@ -5,7 +5,7 @@ echo Deploying ...
 cd $TRAVIS_BUILD_DIR/Environments/Production
 sudo docker-compose -f build.yml build
 sudo docker login --username=$DOCKERHUB_USER --password=$DOCKERHUB_PASSWORD
-sudo docker push dulce/user-client:prod
-sudo docker push dulce/user-server:prod
+sudo docker push dulce/schedule-client:prod
+sudo docker push dulce/schedule-server:prod
 sudo docker run cdrx/rancher-gitlab-deploy upgrade --rancher-url $RANCHER_URL --rancher-key $RANCHER_ACCESS_KEY --rancher-secret $RANCHER_SECRET_KEY --environment Production --stack Schedule --service scheduleclient --finish-upgrade 
 sudo docker run cdrx/rancher-gitlab-deploy upgrade --rancher-url $RANCHER_URL --rancher-key $RANCHER_ACCESS_KEY --rancher-secret $RANCHER_SECRET_KEY --environment Production --stack Schedule --service scheduleserver --finish-upgrade 
